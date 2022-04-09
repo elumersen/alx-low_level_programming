8468594e48575f022ecfe3118a280c21886931dc
elumersen <melatermiyas2011.com>
2022-04-08 17:28:26 -0700

    append n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,6 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
-
 
 /**
  * main - entry point
@@ -15,29 +11,28 @@
 int main(void)
 
 {
-
 	int n;
 
-
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 
+	if (n % 10 > 5)
+	{
+		printf("Last didgit of %i is %i and is greater than 5\n", n , n % 10);
+	}
 
+	else if (n % 10 == 0)
+	{
+		printf("last digit of %i is %i  and is 0\n", n , n % 10);
+	}
 
-	printf("Last digit of %d is %d %s\n", n, n % 10,
-
-			((n % 10) == 0) ? "and is 0"
-
-			: (((n % 10) > 5) ? "and is greater than 5"
-
-				: "and is less than 6 and not 0"));
-
+	else
+	{
+		printf("last digit of %i is %i and is less than 6 and not 0\n", n , n % 10);
+	}
 
 
 	return (0);
-
 
 }
 
