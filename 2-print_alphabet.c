5ce8f30b3c3534f42d27938019d3765595e5a759
elumersen <melatermiyas2011.com>
2022-04-08 19:00:41 -0700

    done

---
+++ 2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Success 0
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+
+
+	for (c = 0; c < 26; ++c)
+
+		putchar('a' + c);
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+
+}
+
