9373dd6b2d148045609feb14d3321755183ccc61
elumersen <melatermiyas2011.com>
2022-04-08 17:47:03 -0700

    append n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,30 +8,22 @@
 
 /**
  *
- *  * main - entry point
+ * main - entry point
  * 
+ * Description: Prints a number and it's last digit along with whether its
  *
- *    * Description: Prints a number and it's last digit along with whether its
+ * last digit is 0, greater than 5, or less than 6 and not 0.
  *
- *     * last digit is 0, greater than 5, or less than 6 and not 0.
- *
- *      * Return: 0
- *
+ *  Return: 0
  */
 
 int main(void)
 
 {
-
 	int n;
 
-
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-
-
 
 	printf("Last digit of %d is %d %s\n", n, n % 10,
 
@@ -42,8 +34,6 @@ int main(void)
 				: "and is less than 6 and not 0"));
 
 
-
 	return (0);
-
 
 }
