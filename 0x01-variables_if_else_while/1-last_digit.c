412331d168f2e8898bde95a500660d6e4cefa1e1
elumersen <melatermiyas2011.com>
2022-04-08 17:38:46 -0700

    append n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,14 +23,13 @@ int main(void)
 
 	else if (n % 10 == 0)
 	{
-		printf("last digit of %i is %i  and is 0\n", n , n % 10);
+		printf("last digit of %i is %i and is 0\n", n , n % 10);
 	}
 
 	else
 	{
 		printf("last digit of %i is %i and is less than 6 and not 0\n", n , n % 10);
 	}
-
 
 	return (0);
 
