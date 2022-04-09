9e7f8fac761491f8cd5bc9e73f1ac96fe3ace473
elumersen <melatermiyas2011.com>
2022-04-09 06:39:10 -0700

    task 1 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: thus is 
+ *
+ * return: 0 success
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+
+
+	for (c = 0; c < 26; ++c)
+
+		putchar('a' + c);
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
