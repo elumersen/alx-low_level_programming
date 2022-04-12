e7f4e118e74010b945bd2da59d2f6ddc0fd06048
elumersen <melatermiyas2011.com>
2022-04-12 06:35:38 -0700

    task 3 is updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,34 +2,25 @@
 /**
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
+ *@c: The int to print
+ *
  * Rturn: Always 0.
  */
+
 int _isalpha(int c)
-
-
-
 {
-
-
-
 	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 
 	{
-
 		return (1);
-
 	}
-
-
 
 	else
 
-	{
-
+ 	{
 		return (0);
-
 	}
 
 
 
-
+}
