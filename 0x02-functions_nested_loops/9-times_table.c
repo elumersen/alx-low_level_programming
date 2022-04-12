3cb39e0cd13579a663e3f20aba4c546a1ae09b6f
elumersen <melatermiyas2011.com>
2022-04-12 07:14:42 -0700

    task 9 updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,7 @@
 void times_table(void)
 {
 	int a;
-
 	int b;
-
 	int c;
 
 	for (a = 0; a <= 9; a++)
@@ -17,8 +15,8 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-
-			if ((c / 10) == 0)			{
+			if ((c / 10) == 0)
+			{
 				if (b == 0)
 				{
 					_putchar ('0');
@@ -45,7 +43,6 @@ void times_table(void)
 				}
 			}
 		}
-
 		_putchar ('\n');
 	}
 }
