0ab5d5c12f05f89016ebbfdc3ba2b23d762d0131
elumersen <melatermiyas2011.com>
2022-04-12 14:32:08 -0700

    advanced task 13 done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,13 +12,11 @@ int main(void)
 
 		int i, res;
 
-
-
 			for (i = 0; i < 1024; i++)
 
-						if (i % 3 == 0 || i % 5 == 0)
+				if (i % 3 == 0 || i % 5 == 0)
 
-										res += i;
+					res += i;
 
 				printf("%d\n", res);
 
