b6c18e0ba68009dbe1fc140c76d6bf3909258cd7
vboxuser <vboxuser@Ubuntu22.04.myguest.virtualbox.org>
2023-09-08 17:47:38 -0300

    It's done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: This program will add two numbers
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+
+
+	for (c = 0; c < 26; ++c)
+
+		putchar('a' + c);
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Print lower case alphabate excluding e and q
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; ++c)
+
+	{
+
+		if (c != 'e' && c != 'q')
+
+			putchar(c);
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
